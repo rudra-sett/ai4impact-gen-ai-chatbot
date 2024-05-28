9ec003ee08ac533a6ab484072a97f2c52c21778b
RachelKahnAI4Impact <157506206+RachelKahnAI4Impact@users.noreply.github.com>
2024-05-28 15:43:08 -0400

    updated constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,11 +2,11 @@ export const AUTHENTICATION = true;
 
 // change these as needed
 // kendraIndexName - must be unique to your account
-export const kendraIndexName = 'gen-ai-chatbot-index'
+export const kendraIndexName = 'eea-grants-navigator-index'
 // must be unique globally or the deployment will fail
-export const cognitoDomainName = "genaimvp-auth"
+export const cognitoDomainName = "genaimvp-auth-eea"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
 export const OIDCIntegrationName = "Azure-OIDC"
 // this MUST be unique to your account
-export const stackName = "GenAiChatStack"
+export const stackName = "GenAiChatStack-eea"
