78602d7a043f4e5179e4f663653180119e64e314
Rudra Sett <rudro.sett@gmail.com>
2024-10-03 15:13:59 -0400

    changed consts

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,10 +2,10 @@ export const AUTHENTICATION = true;
 
 // change these as needed
 // must be unique globally or the deployment will fail
-export const cognitoDomainName = "genaimvp-auth"
+export const cognitoDomainName = "glo-test-auth"
 // this can be anything that would be understood easily, but you must use the same name
 // when setting up a sign-in provider in Cognito
 // make sure to leave it blank if you do not actually have an SSO provider configured in Cognito!
 export const OIDCIntegrationName = ""
 // this MUST be unique to your account
-export const stackName = "GenAiChatStack"
+export const stackName = "glo-test-stack"
