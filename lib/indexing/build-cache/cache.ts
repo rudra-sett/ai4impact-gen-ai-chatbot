267b1177d658598fd8999e42ca94940bf2b8161f
Rudra Sett <rudro.sett@gmail.com>
2025-01-06 17:48:49 -0500

    just use the ASL version

--- lib/indexing/build-cache/cache.ts
+++ lib/indexing/build-cache/cache.ts
@@ -13,7 +13,7 @@ export class CacheAmendments extends Construct {
   constructor(scope: Construct, id: string, props: CacheAmendmentsStackProps) {
     super(scope, id);
 
-    const s3Map = new sfn.DistributedMap(this, 'S3 Map', {
+    /*const s3Map = new sfn.DistributedMap(this, 'S3 Map', {
       maxConcurrency: 4,
       itemReader: new sfn.S3ObjectsItemReader({ bucket: props.actsBucket, prefix: 'acts/' }),
       label: 'ActBucketMap',
@@ -55,11 +55,75 @@ export class CacheAmendments extends Construct {
       },
     });
 
-    s3Map.itemProcessor(lambdaInvoke);
+    s3Map.itemProcessor(lambdaInvoke); */
+
+    const jsonDef = {
+      "QueryLanguage": "JSONata",
+      "Comment": "State Machine to cache all amendments",
+      "StartAt": "S3 object keys",
+      "States": {
+        "S3 object keys": {
+          "Type": "Map",
+          "ItemProcessor": {
+            "ProcessorConfig": {
+              "Mode": "DISTRIBUTED",
+              "ExecutionType": "STANDARD"
+            },
+            "StartAt": "Lambda Invoke",
+            "States": {
+              "Lambda Invoke": {
+                "Type": "Task",
+                "Resource": "arn:aws:states:::lambda:invoke",
+                "Output": "{% $states.result.Payload %}",
+                "Arguments": {
+                  "FunctionName": props.amendmentFunction.functionName,
+                  "Payload": {
+                    "body": {
+                      "year": "{% $split($states.input.Key, '/')[1] %}",
+                      "chapter": "{% $substringBefore($substringAfter($split($states.input.Key, '/')[2], 'chapter-'), '.txt') %}"
+                    }
+                  }
+                },
+                "Retry": [
+                  {
+                    "ErrorEquals": [
+                      "Lambda.ServiceException",
+                      "Lambda.AWSLambdaException",
+                      "Lambda.SdkClientException",
+                      "Lambda.TooManyRequestsException"
+                    ],
+                    "IntervalSeconds": 1,
+                    "MaxAttempts": 3,
+                    "BackoffRate": 2,
+                    "JitterStrategy": "FULL"
+                  }
+                ],
+                "End": true
+              }
+            }
+          },
+          "ItemReader": {
+            "Resource": "arn:aws:states:::s3:listObjectsV2",
+            "Arguments": {
+              "Bucket": props.actsBucket.bucketName,
+              "Prefix": "acts/"
+            }
+          },
+          "MaxConcurrency": 4,
+          "Label": "S3objectkeys",
+          "End": true,
+          "ToleratedFailurePercentage": 50
+        }
+      }
+    }
 
     // Define the state machine
     const stateMachine = new sfn.StateMachine(this, 'StateMachine', {
-      definition: s3Map,      
+      definitionBody: sfn.DefinitionBody.fromString(JSON.stringify(jsonDef)),            
     });
+
+    props.actsBucket.grantRead(stateMachine);
+    props.amendmentFunction.grantInvoke(stateMachine);
+    
   }
 }
